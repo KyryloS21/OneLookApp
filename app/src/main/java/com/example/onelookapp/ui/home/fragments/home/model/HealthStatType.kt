b1bc46e663164e54a159edb46f0e324af63824f0
KyryloS21 <k.s.skhodnytsjkyy@student.csn.khai.edu>
2025-01-03 16:38:01 +0200

    feat: створення адаптера та классу HealsStats

---
+++ app/src/main/java/com/example/onelookapp/ui/home/fragments/home/model/HealthStatType.kt
@@ -0,0 +1,8 @@
+package com.example.onelookapp.ui.home.fragments.home.model
+
+enum class HealthStatType(val type:String) {
+    BREATH("breath"),
+    HEART_RATE("heart_rate"),
+    BLOOD_PRESSURE("blood_pressure")
+
+}
